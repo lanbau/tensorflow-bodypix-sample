ce0cfb21a95f102f95cb7c27b0e9f558ef61d312
Irvin <irvinln@gmail.com>
2022-04-13 23:38:57 +0800

    updates

---
+++ components/Webcam.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import Webcam from "react-webcam";
+
+
+const WebcamComponent = () => <Webcam />;
+
+const videoConstraints = {
+    width: 220,
+    height: 200,
+    facingMode: "user"
+};
+
+export const WebcamCapture = ({ image,setImage }) => {
+
+    // const [image,setImage]=useState('');
+    const webcamRef = React.useRef(null);
+
+    
+    const capture = React.useCallback(
+        () => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImage(imageSrc)
+        
+        },[]);
+
+        console.log("DEBUG: inage", image)
+
+    return (
+        <div className="webcam-container">
+            <div className="webcam-img">
+
+                {image == '' ? <Webcam
+                    audio={false}
+                    height={200}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width={220}
+                    videoConstraints={videoConstraints}
+                // eslint-disable-next-line @next/next/no-img-element
+                /> : <img src={image} />}
+            </div>
+            <div>
+                {image != '' ?
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        setImage('')
+                    }}
+                        className="webcam-btn">
+                        Retake Image</button> :
+                    <button onClick={(e) => {
+                        e.preventDefault();
+                        capture();
+                    }}
+                        className="webcam-btn">Capture</button>
+                }
+            </div>
+        </div>
+    );
+};
